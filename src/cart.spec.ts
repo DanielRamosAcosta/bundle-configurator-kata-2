95838ed01c03a75401f1f29f6b8fe5607adcbfc9
Luis Rovirosa Mairlot <luis@codium.team>
2022-04-29 18:55:25 +0200

    returns the two products if is not possible to create a bundle

--- src/cart.spec.ts
+++ src/cart.spec.ts
@@ -12,7 +12,17 @@ describe("cart", () => {
     expect(bundles).toEqual([p1])
   })
 
-  it.todo("returns the two products if is not possible to create a bundle")
+  it("returns the two products if is not possible to create a bundle", () => {
+    const cart = new Cart()
+    const p1 = new Product("p1")
+    const p3 = new Product("p3")
+    cart.add(p1)
+    cart.add(p3)
+
+    const bundles = cart.computeBundles();
+
+    expect(bundles).toEqual([p1, p3])
+  })
 
   it.todo("creates a bundle if is possible")
 })
